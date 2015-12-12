d0deefd124073dc7dfeea56052e1a5e74ea4f67a
zhoushengxian <zhoushengxian@richinfo.cn>
2015-12-12 15:01:21 +0800

    1、 2.5.2 singleTop 的测试前奏   自行测试 singleTop的 onPause,onStop, onDestroy 等等事件

--- 2/ActivityTest/app/src/main/java/com/example/zhoushx/activitytest/FirstActivity.java
+++ 2/ActivityTest/app/src/main/java/com/example/zhoushx/activitytest/FirstActivity.java
@@ -157,11 +157,49 @@ public class FirstActivity extends Activity {
         }
     }
 
-
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String tempData = "Something you just typed";
+        outState.putString("data_key", tempData);
+
+        Log.d(TAG, "SavedData before OnStop activity|data_key=" + tempData);
     }
 
 }
